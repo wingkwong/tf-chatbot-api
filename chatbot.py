ef23eb3127306140492e11e5e3d7060167db1d24
wingkwong <wingkwong.code@gmail.com>
2018-03-09 00:17:36 +0800

    draft chatbot

---
+++ chatbot.py
@@ -0,0 +1,32 @@
+import config
+
+
+def train():
+	print("training bot..")
+
+	# retrieve config
+	c = config.getConfig()
+	print(c.get('DATASET', 'DATA_PATH'))
+
+	# build the chatbot model
+	## TODO 
+
+	# Create a saver
+	## TODO
+	### Ref: https://www.tensorflow.org/api_docs/python/tf/train/Saver
+
+
+	# run tf session
+	## TODO
+	### Ref: https://www.tensorflow.org/api_docs/python/tf/Session
+
+
+
+def main():
+	print("chatbot.py starts..")
+	train()
+	print("chatbot.py ends..")
+
+
+if __name__ == '__main__':
+    main()
