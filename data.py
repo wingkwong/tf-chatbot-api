6743ebb3e596a9c0d2e0977918d23ea2b54b4f06
wingkwong <wingkwong.code@gmail.com>
2018-03-11 00:16:55 +0800

    added get_questions_answers

--- data.py
+++ data.py
@@ -35,7 +35,32 @@ def get_convs():
                 convs.append(conv)
     return convs
 
+def get_questions_answers(id2line, convs):
+	print('getting questions and answers')
+	questions, answers = [], []
+	# create sets for checking if q & a have already been added or not
+	added_questions, added_answers = set(), set()
+	duplicated = 0;
+	for conv in convs:
+		for i, line in enumberate(conv[:-1]):
+			# q: odd & a: even
+			if convs[i+1] in id2line or not convs[i] in id2line:
+				continue
+			question = id2line[conv[i]]
+			anwser = id2line[conv[i+1]]
+			# skip duplicated cases
+			if question in added_questions or answer in added_answers:
+				duplicated += 1
+				continue
+			questions.append(question)
+			added_questions.append(question)
+			answers.append(answer)
+			added_answers.append(answer)
+	print('duplicated: ', duplicated)
+	return questions, answers
+
 
 def load():
 	id2line = get_id2line()
 	convs = get_convs()
+	questions, answers = get_questions_answers(id2line, convs)
